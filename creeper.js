86a9f1dd40eba3d317f86c5699229c6ca1de51cf
Bruna <psbru2012@gmail.com>
2023-03-21 13:39:41 -0300

    exercício creeper

---
+++ creeper.js
@@ -0,0 +1,18 @@
+var tela = document.querySelector('canvas');
+var pincel = tela.getContext('2d');
+
+pincel.fillStyle = "darkgreen";
+pincel.fillRect(0, 0, 350, 300);
+
+pincel.fillStyle = "black";
+pincel.fillRect(50, 60, 90, 90);
+
+pincel.fillRect(210, 60, 90, 90);
+
+pincel.fillRect(140, 150, 70, 100);
+
+pincel.fillRect(90, 210, 50, 90);
+
+pincel.fillRect(210, 210, 50, 90);
+
+
